262b9931f219b975eaa069040923c4105151ddcc
kzeca <jcpeixotoleao@hotmail.com>
2020-03-22 19:31:39 -0400

    POC login

---
+++ CheckApp/app/src/main/java/com/fmm/checkapp/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.fmm.checkapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- CheckApp/app/src/main/java/com/fmm/checkapp/MainActivity.java
+++ CheckApp/app/src/main/java/com/fmm/checkapp/MainActivity.java
@@ -1,14 +1,66 @@
 package com.fmm.checkapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btlogin;
+    EditText email;
+    EditText password;
+    private FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btlogin = findViewById(R.id.bt_Login);
+        email = findViewById(R.id.txt_email);
+        password = findViewById(R.id.txt_password);
+        auth  = FirebaseAuth.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(email.getText().toString(), password.getText().toString());
+            }
+        });
     }
+
+    private void login(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            System.out.println("deu bom!");
+                            firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                        else{
+                            System.out.println("Deu ruiM!");
+                            Toast.makeText(MainActivity.this, "Errou!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
 }
