6a07408f2d522768e7d618f875d4883e09cee980
kzeca <jcpeixotoleao@hotmail.com>
2020-03-22 22:45:56 -0400

    Add user Authentication

--- CheckApp/app/src/main/java/com/fmm/checkapp/LoginActivity.java
+++ CheckApp/app/src/main/java/com/fmm/checkapp/LoginActivity.java
@@ -1,12 +1,24 @@
 package com.fmm.checkapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,21 +27,40 @@ public class LoginActivity extends AppCompatActivity {
     TextView tvEsqueceuSenha;
     EditText edtEmail;
     EditText edtSenha;
+    private FirebaseAuth auth;
+    FirebaseUser firebaseUser;
+    ProgressBar progressBar;
+    static Usuario user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        user = new Usuario();
         btLogin = findViewById(R.id.activity_login_bt_login);
         tvEsqueceuSenha = findViewById(R.id.activity_login_tv_esqueceu_senha);
         tvIrTelaCadastro = findViewById(R.id.activity_login_tv_cadastrar);
         edtEmail = findViewById(R.id.activity_login_edt_email);
         edtSenha = findViewById(R.id.activity_login_edt_senha);
+        progressBar = findViewById(R.id.activity_login_progressBar);
+        auth  = FirebaseAuth.getInstance();
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         btLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                user.setEmail(edtEmail.getText().toString());
+                user.setSenha(edtSenha.getText().toString());
+                
+                if(user.getEmail().isEmpty() || user.getSenha().isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Os campos de email e senha são obrigatórios",
+                            Toast.LENGTH_SHORT).show();
+                }else {
+                    String email = user.getEmail();
+                    String senha = user.getSenha();
+                    progressBar.setVisibility(View.VISIBLE);
+                    login(email, senha);
+                }
             }
         });
 
@@ -47,4 +78,52 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void login(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+                            Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                            startActivity(intent);
+                            progressBar.setVisibility(View.GONE);
+                            finish();
+                        }
+                        else{
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(LoginActivity.this, "Email e/ou senha incorretos",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    private TextView.OnEditorActionListener editorListener = new TextView.OnEditorActionListener() {
+        @Override
+        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+
+            user.setEmail(edtEmail.getText().toString());
+            user.setSenha(edtSenha.getText().toString());
+
+            switch (actionId) {
+                case EditorInfo.IME_ACTION_SEND:
+                    if(user.getEmail().isEmpty() || user.getSenha().isEmpty()){
+                        Toast.makeText(getApplicationContext(),
+                                "Os campos de email e senha são obrigatórios",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    }else {
+                        String email = user.getEmail();
+                        String password = user.getSenha();
+                        progressBar.setVisibility(View.VISIBLE);
+                        login(email, password);
+                        break;
+                    }
+            }
+            return false;
+        }
+    };
+
 }
---
+++ CheckApp/app/src/main/java/com/fmm/checkapp/Usuario.java
@@ -0,0 +1,53 @@
+package com.fmm.checkapp;
+
+import android.widget.Adapter;
+
+public class Usuario {
+
+    private String nome;
+    private String email;
+    private String senha;
+    private String uId;
+    private String turma;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getuId() {
+        return uId;
+    }
+
+    public void setuId(String uId) {
+        this.uId = uId;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+}
